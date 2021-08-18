e11a0bddd17e435a4afad993528dd98366258996
Darlley <systembrtt@gmail.com>
2021-08-18 12:26:18 -0400

    aula 01 - exercício 3

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,16 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+/* Resolvido da forma que estava selecionando o atributo data
+
+document.querySelector('[data-js="app"]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+
+
+ja adianto que o AdjacentHTML eu ja tinha conhecido esta semana n curso de JavaScript para React do Marco Bruno que estou acompanhando : */
+
+
+const $mensagem = `<h1>B. Academy</h1>
+<p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+document.querySelector('[data-js="app"]').insertAdjacentHTML("beforeend", $mensagem);
